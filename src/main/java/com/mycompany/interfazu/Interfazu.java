a164817448b156cc8642db3c33aa0913afc0e433
Cristamd88 <relavgam@gmail.com>
2022-12-27 20:13:12 -0600

    Primer commit

---
+++ src/main/java/com/mycompany/interfazu/Interfazu.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.interfazu;
+
+import GUI.Ventana1;
+
+/**
+ *
+ * @author ASUS455
+ */
+public class Interfazu {
+
+    public static void main(String[] args) {
+        Ventana1 v1 = new Ventana1();
+        v1.setVisible(true);
+    }
+}
